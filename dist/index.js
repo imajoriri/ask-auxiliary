ca4ab54e3b9bceec25459c5ea781ae3fd7748aac
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-05-16 15:57:02 +0900

    update index.js add getSlotId function

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,19 @@ AskAuxiliary.prototype = {
     }
   },
 
+  // id取得
+  getSlotsId: function(handlerInput, key){
+    var request = handlerInput.requestEnvelope.request;
+    var slots = request.intent.slots[key];
+    if(slots === undefined){
+      throw new Error("not defined " + key + " slots");
+    }else if(slots.resolutions.resolutionsPerAuthority[0].values === undefined){
+      throw new Error(key + " の類義語設定してない場合は[getSlot]メソッドを使用してね");
+    }else{
+      return slots.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+    }
+  },
+
 }
 
 module.exports = new AskAuxiliary();
