72adf621627afc16381cbe7694dd4fc421b30682
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-05-16 15:26:44 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+function AskAuxiliary(){
+}
+
+AskAuxiliary.prototype = {
+  // 一般的なスロット取得方法
+  getSlot: function(handlerInput, key){
+    var request = handlerInput.requestEnvelope.request;
+    var slots = request.intent.slots[key];
+    if(slots === undefined){
+      throw new Error("not defined " + key + " slots");
+    }else{
+      return slots.value;
+    }
+  },
+
+  // 類義語を設定している時
+  getFixedSlot: function(handlerInput, key){
+    var request = handlerInput.requestEnvelope.request;
+    var slots = request.intent.slots[key];
+    if(slots === undefined){
+      throw new Error("not defined " + key + " slots");
+    }else if(slots.resolutions.resolutionsPerAuthority[0].values === undefined){
+      throw new Error(key + " の類義語設定してない場合は[getSlot]メソッドを使用してね");
+    }else{
+      return slots.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    }
+  },
+
+}
+
+module.exports = new AskAuxiliary();
